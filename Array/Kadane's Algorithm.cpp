78bd6e52e60a07491195a9e67bdb99bc6c7c45c1
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-13 11:03:35 +0530

    Create Kadane's Algorithm.cpp

---
+++ Array/Kadane's Algorithm.cpp
@@ -0,0 +1,33 @@
+ link-https://practice.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+
+Input:
+N = 5
+arr[] = {1,2,3,-2,5}
+Output:
+9
+Explanation:
+Max subarray sum is 9
+of elements (1, 2, 3, -2, 5) which 
+is a contiguous subarray.
+  
+  ``````
+  
+  
+  
+class Solution{
+    public:
+     
+    int maxSubarraySum(int arr[], int n){
+        int sum=0;
+        int maxi=INT_MIN;
+        for(int i=0;i<n;i++){
+            sum=sum+arr[i];
+            maxi=max(sum,maxi);
+            if(sum<0){
+                sum=0;
+            }
+        }
+        return maxi;
+        
+    }
+};
