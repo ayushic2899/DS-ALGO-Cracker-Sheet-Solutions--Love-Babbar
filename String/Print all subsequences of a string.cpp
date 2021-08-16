2378c7ec119509adce84f48ce66bef44a66af9f3
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-16 15:17:37 +0530

    Create Print all subsequences of a string.cpp

---
+++ String/Print all subsequences of a string.cpp
@@ -0,0 +1,28 @@
+Input : abc
+Output : a, b, c, ab, bc, ac, abc
+
+Input : aaa
+Output : a, aa, aaa
+
+``````
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void func(string t,int i,int n,string s){
+	if(i==n){
+		cout<<t<<endl;
+	}
+	else{
+		func(t,i+1,n,s);
+		t=t+s[i];
+		func(t,i+1,n,s);
+	}
+}
+
+int main(){
+	string s;
+	cin>>s;
+	func("",0,s.length(),s);
+	return 0;
+}
