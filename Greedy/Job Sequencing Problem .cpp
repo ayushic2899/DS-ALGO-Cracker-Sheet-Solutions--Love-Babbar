7710a2f49b368c6fa7d95091e12d96c85a3b2943
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-17 16:26:05 +0530

    Create Job Sequencing Problem .cpp

---
+++ Greedy/Job Sequencing Problem .cpp
@@ -0,0 +1,40 @@
+link-https://practice.geeksforgeeks.org/problems/job-sequencing-problem-1587115620/1
+
+```````
+
+class Solution 
+{
+    public:
+    
+    static bool myCmp(Job a, Job b)
+    {
+    return a.profit > b.profit;
+    }
+
+    vector<int> JobScheduling(Job arr[], int n) 
+    { 
+        
+        sort(arr, arr+n, myCmp);
+    
+        bool done[n] = {0};
+    
+       int day = 0, profit = 0;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = min(n,arr[i].dead-1); j >= 0; j--)
+        {
+            if(done[j] == false)
+            {
+                day += 1;
+                profit += arr[i].profit;
+                
+                done[j] = true;
+                
+                break;
+            }
+        }
+    }
+    
+     return {day,profit};
+    } 
+};
