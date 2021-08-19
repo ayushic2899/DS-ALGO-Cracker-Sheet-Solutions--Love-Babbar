bbbc890e1a076c44ba1619ca9a1e4bb9a22f33c9
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-19 18:17:13 +0530

    Create Edit Distance .cpp

---
+++ Dynamic programming/Edit Distance .cpp
@@ -0,0 +1,47 @@
+Insert
+Remove
+Replace
+ 
+
+Example 1:
+
+Input: 
+s = "geek", t = "gesek"
+Output: 1
+Explanation: One operation is required 
+inserting 's' between two 'e's of str1.
+
+
+```````
+
+
+class Solution {
+  public:
+    int dp[1001][1001];
+    int func(string s,string t,int m,int n){
+        if(m==-1){
+            return n+1;
+        }
+        if(n==-1){
+            return m+1;
+        }
+        if(dp[m][n] != -1){
+            return dp[m][n];
+        }
+        if(s[m]==t[n]){
+            return dp[m][n]=func(s,t,m-1,n-1);
+        }
+        int a=func(s,t,m-1,n-1);
+        int b=func(s,t,m,n-1);
+        int c=func(s,t,m-1,n);
+        
+        return dp[m][n]=1+min(a,min(b,c));
+    }
+    int editDistance(string s, string t) {
+        // Code here
+        memset(dp,-1,sizeof(dp));
+        int m=s.length();
+        int n=t.length();
+        return func(s,t,m-1,n-1);
+    }
+};
