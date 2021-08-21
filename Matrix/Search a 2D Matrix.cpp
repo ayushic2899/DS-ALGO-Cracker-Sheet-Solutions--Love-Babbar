bcf5f99e87f6b26df31075fea078779c998f79d4
Ayushi choudhary <67158285+ayushic2899@users.noreply.github.com>
2021-08-22 00:42:30 +0530

    Create  Search a 2D Matrix.cpp

---
+++ Matrix/Search a 2D Matrix.cpp
@@ -0,0 +1,28 @@
+link-https://leetcode.com/problems/search-a-2d-matrix/
+
+````````````
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        int low=0;
+        int high=(n*m)-1;
+        while(low<=high){
+            int mid=(low+(high-low)/2);
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+            
+        }
+        return false;
+    }
+};
